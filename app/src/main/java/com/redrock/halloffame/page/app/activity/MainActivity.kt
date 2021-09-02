cdcdc2bb29690e59ec2d99989c6f044602bc3ee3
985892345 <2767465918@qq.com>
2021-09-02 14:54:50 +0800

    [feature]项目启动_2021.9.2-15:00

---
+++ app/src/main/java/com/redrock/halloffame/page/app/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.redrock.halloffame.page.app.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.redrock.halloffame.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
